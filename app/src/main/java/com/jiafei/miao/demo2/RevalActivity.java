fc9ad55b0c8ccfd4a716d838defb6d5ba97de9d7
JiaFeiMiao <jiafeimiao18@gmail.com>
2016-03-08 09:02:31 +0800

    ok

---
+++ app/src/main/java/com/jiafei/miao/demo2/RevalActivity.java
@@ -0,0 +1,18 @@
+package com.jiafei.miao.demo2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jiafei.miao.R;
+
+/**
+ * Created by zouyingjie on 16/3/8.
+ */
+public class RevalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reval);
+    }
+}

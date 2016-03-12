751e09d9acbe4b76c5ae53dd5578b44330d67d50
JiaFeiMiao <jiafeimiao18@gmail.com>
2016-03-12 16:54:14 +0800

    add reval animation and vector

---
+++ app/src/main/java/com/jiafei/miao/contants/Contant.java
@@ -0,0 +1,11 @@
+package com.jiafei.miao.contants;
+
+/**
+ * Created by zouyingjie on 16/3/10.
+ */
+public class Contant {
+
+    public static final String TRAN_TAG = "TRAN_TAG";
+    public static final String TRAN_BY_JAVA = "JAVA";
+    public static final String TRAN_BY_XML = "XML";
+}
